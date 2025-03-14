6df206af6e3962ec4d1f18828ab494ee9bc2efe2
Chore Lemming <chore8_lemming@icloud.com>
2025-03-13 22:31:14 -0400

    VDIFReceiver: Add time calculation and frequency axis

--- vdif/VDIFReceiver.py
+++ vdif/VDIFReceiver.py
@@ -7,6 +7,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import io
 # import baseband
 from baseband import vdif
+from datetime import datetime, timedelta
 
 class VDIFReceiver:
     def __init__(self):
@@ -53,6 +54,8 @@ class VDIFReceiver:
         
         self.buffer = io.BytesIO()
         self.fh = vdif.open(self.buffer, 'rb')
+        self.frame_rate = 0
+        self.sample_rate = 0
         self.buffer_frame = None
 
         # Create a matplotlib figure and axis
@@ -63,6 +66,7 @@ class VDIFReceiver:
     def start_receiving(self):
         port = int(self.port_entry.get())
         output_file = self.file_entry.get()
+        print(f"Listening on port {port} and writing to " + output_file)
 
         # Create a UDP socket
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -84,6 +88,8 @@ class VDIFReceiver:
             self.status_label.config(text="Receiver stopped.")
         self.start_button.config(state=tk.NORMAL)
         self.stop_button.config(state=tk.DISABLED)
+        self.frame_rate = 0
+        print("Stopped receiving ")
 
     def receive_data(self, output_file):
         with open(output_file, 'wb') as f:
@@ -93,10 +99,19 @@ class VDIFReceiver:
                 try:
                     data, addr = self.socket.recvfrom(4096)  # Buffer size is 4096 bytes
                     if data:
-                        f.write(data)
                         print(f"Received {len(data)} bytes from {addr}")
+                        f.write(data)
                         self.data.append(len(data))  # Store the length of received data
                         frame_count += 1
+                        
+                        if self.frame_rate == 0:
+                            self.buffer.seek(0)
+                            self.buffer.write(data)
+                            self.buffer.seek(0)
+                            self.buffer_frame = self.fh.read_frame()
+                            print(self.fh.info())
+                            self.frame_rate = np.float64(self.fh.info()['frame_rate'])
+                            self.sample_rate = np.float64(self.fh.info()['sample_rate'])
 
                         # Call process_data every N frames
                         if frame_count % 100 == 0:  # Change 10 to any N you want
@@ -113,24 +128,51 @@ class VDIFReceiver:
                     print(f"Error receiving data: {e}")
 
         self.status_label.config(text="Data written to " + output_file)
+        print("Data written to " + output_file)
 
     def process_data(self):
         # Update the plot with the current data
         # print(baseband.file_info(self.buffer))
         self.ax.clear()
         frame_data = self.buffer_frame.data
-        self.ax.plot(10*np.log10(np.abs(np.fft.fft(frame_data, axis=0))))
+        x_Hz = np.fft.fftshift(np.fft.fftfreq(len(frame_data),d=1/self.sample_rate))
+        self.ax.plot(x_Hz/1e6, 10*np.log10(np.abs(np.fft.fft(frame_data, axis=0))))
         self.ax.set_title("FFT for " + 
                           f"Thread {self.buffer_frame.header['thread_id']} " + 
-                          f"Frame {self.buffer_frame.header['frame_nr']}")
-        self.ax.set_xlabel('Frame Number')
+                          str(self.calculate_first_sample_time(frame=self.buffer_frame)))
+        self.ax.set_xlabel('Frequency (MHz)')
         self.ax.set_ylabel('dB')
         # self.ax.legend()
         self.canvas.draw()
 
     def run(self):
         self.root.mainloop()
+        
+    # Function to calculate the time of the first sample in a VDIF frame
+    def calculate_first_sample_time(self, frame):
 
+        # Get the epoch (half years since 2000) and seconds from epoch
+        epoch = frame['ref_epoch']
+        whole_seconds_from_epoch = frame['seconds']
+        
+        # Get fractional seconds using frame number and frame rate
+        seconds_from_epoch = whole_seconds_from_epoch + \
+            frame['frame_nr']/self.frame_rate
+        
+        # Set the epoch
+        years = epoch // 2
+        half_year = epoch % 2
+        
+        if half_year == 0:
+            base_date = datetime(2000 + years, 1, 1)
+        else:
+            base_date =  datetime(2000 + years, 7, 1)
+        
+        # Calculate the time of the first sample
+        first_sample_time = base_date + timedelta(seconds=seconds_from_epoch)
+        
+        return first_sample_time
+        
 if __name__ == "__main__":
     receiver = VDIFReceiver()
     receiver.run()
